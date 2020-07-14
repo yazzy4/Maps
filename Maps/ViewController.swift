f75b1c1828ab68039c4964153ef11c2fe4bcdc40
yazzy4 <yazburrell@pursuit.org>
2020-07-14 15:12:20 -0400

    Set up basic UI for map. Camera button connected and has basic function

--- Maps/ViewController.swift
+++ Maps/ViewController.swift
@@ -7,14 +7,21 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      
     }
 
-
+    @IBAction func didTapCameraButton(_ sender: UIButton) {
+        print("say cheese!")
+    }
+    
 }
 
